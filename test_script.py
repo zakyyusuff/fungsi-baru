7a643267dd5da74def42f34def2f5a2a0a184f10
zaky yusuf <59588009+zakyyusuff@users.noreply.github.com>
2022-11-17 18:00:49 +0700

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,1 +1,24 @@
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.feature_extraction.text import TfidfVectorizer
+from zakar import *
 
+def test_pred(text):
+    stemmer = StemmerFactory().create_stemmer()
+    # stem = stemmer.stem(text)
+
+    vectorizer = TfidfVectorizer(max_features=1000, decode_error='ignore')
+    # vectorizer.fit(text['text_stemming'])
+    vectorizer.fit(text, stemmer)
+
+
+    # class_pred = pickle_model.predict(vectorizer.transform([stem]))
+    if text == 0:
+        pred = 'Normal'
+        status = 'text-success'
+    elif text == 1:
+        pred = 'Penipuan'
+        status = 'text-danger'
+    else:
+        pred = 'Promo'
+        status = 'text-warning'
+    assert pred, status
